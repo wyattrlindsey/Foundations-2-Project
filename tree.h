4511a52a6996fff534fa4a73338ef828d42610e2
wyattrlindsey <142817045+wyattrlindsey@users.noreply.github.com>
2024-04-27 15:26:15 -0400

    Fixing errors

--- tree.h
+++ tree.h
@@ -11,4 +11,4 @@ Node* createNew(int data);
 
 void rotateLeft(struct Node *node, struct Node **r);
 
-void rotateRight(struct node *n, struct node **root){};
+void rotateRight(struct Node *n, struct Node **root);
