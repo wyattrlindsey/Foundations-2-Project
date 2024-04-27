5a37c5b665d1bac394c81ec9aaa2874563f36103
Annelise Rasmussen <annelisecharlotte86@gmail.com>
2024-04-27 14:26:11 -0400

    annelises stuff

--- tree.h
+++ tree.h
@@ -1,12 +1,12 @@
-struct Node {
-	int dat;
-	char color;
+typedef struct Node {
+	int data;
+	int color;
 	struct Node *left;
 	struct Node *right;
 	struct Node *parent;
-};
+} Node;
 
-struct Node createNew(int data) {};
+Node* createNew(int data) {};
 
 void rotateLeft(struct Node *node, struct Node **r){};
 
