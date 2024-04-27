8c17b5a2fb5ab37151a3c8168918c1fa39787844
Rose Hoelzel <rosehoelzel@Roses-MacBook-Pro-2.local>
2024-04-27 14:06:49 -0400

    tree file

--- tree.c
+++ tree.c
@@ -11,7 +11,7 @@ typedef struct Node {
 } Node;
 
 Node *createNew(int data) {
-    Node *newNode = (Node *)malloc(sizeof(Node));
+    Node *newNode = (Node*)malloc(sizeof(Node));
     newNode->data = data;
     newNode->color = 0;
     newNode->left = NULL;
@@ -20,32 +20,52 @@ Node *createNew(int data) {
     return newNode;
 }
 
-void rotateLeft(Node node) {
-  Node parent = node.parent;
-  Node rightChild = node.right;
-
-  node.right = rightChild.left;
-  if (rightChild.left != null) {
-    rightChild.left.parent = node;
-  }
-
-  rightChild.left = node;
-  node.parent = rightChild;
-
-  replaceParentsChild(parent, node, rightChild);
+bool compareStruct(Node node1, Node node2)
+{
+    if ((node1.data == node2.data) && (node1.color == node2.color) && (node1.left == node2.left) && (node1.right == node2.right) && (node1.parent == node2.parent))
+    {
+        return true;
+    }
 }
 
 void rotateLeft(Node node) {
-  Node parent = node.parent;
-  Node rightChild = node.right;
+    Node* parent = node.parent;
+    Node* rightChild = node.right;
 
-  node.right = rightChild.left;
-  if (rightChild.left != null) {
-    rightChild.left.parent = node;
-  }
+    node.right = rightChild->left;
+    if (rightChild->left != NULL) {
+        *(rightChild->left->parent) = node;
+    }
 
-  rightChild.left = node;
-  node.parent = rightChild;
+    *(rightChild->left) = node;
+    node.parent = rightChild;
 
-  replaceParentsChild(parent, node, rightChild);
+    if (parent == NULL) {
+        Node root = *rightChild;
+    } else if (compareStruct(*parent->left, node)) {
+        parent->left = rightChild;
+    } else if (compareStruct(*parent->right, node)) {
+        parent->right = rightChild;
+    } else {
+        printf("ERROR: Node is not a child");
+    }
+
+    if (rightChild != NULL) {
+        rightChild->parent = parent;
+    }
+}
+
+void rotateLeft(Node node) {
+    Node* parent = node.parent;
+    Node* rightChild = node.right;
+
+    node.right = rightChild->left;
+    if (rightChild->left != NULL) {
+        *(rightChild->left->parent) = node;
+    }
+
+    *(rightChild->left) = node;
+    node.parent = rightChild;
+
+    replaceParentsChild(parent, node, rightChild);
 }
