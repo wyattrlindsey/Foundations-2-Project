b319870ebb8e53e47a1c74c621817ecafbbf634c
rasmussen.123 <rasmussen.123@cse-sl7.coeit.osu.edu>
2024-04-28 21:30:18 -0400

    annelise

--- test.c
+++ test.c
@@ -48,14 +48,6 @@ void test_search_multiple_node_tree(void) {
     }
 }
 
-// // Function to perform in-order traversal
-// void in_order_traversal(Node *node) {
-//     if (node == NULL) return;
-//     in_order_traversal(node->left);
-//     printf("%d ", node->data);
-//     in_order_traversal(node->right);
-// }
-// Function to print the red-black tree in inorder traversal
 void in_order_traversal(Node* root) {
     char color_letter = 'R';
     if (root != NULL) {
@@ -72,47 +64,50 @@ void in_order_traversal(Node* root) {
 
 int check_tree(Node* root) {
 
+    //initializing variables for counting black nodes recursively
+    int right_black_nodes = 0;
+    int left_black_nodes = 0;
+
     //root can't be null
     if(root == NULL) {
         printf("\nINSERTION FAILED: root is null");
         return 0;
-    } else {
-        //nodes color has to be 1 or 0 (red or black)
-        if(root->color != 1 && root->color != 0) {
-            printf("INSERTION FAILED: Node #%d has an invalid color type", root->data);
-            return 0;
-        }
-
-        //root node MUST be black
-        if(root->color != 0) {
-            printf("INSERTION FAILED: Root node MUST be black", root->data);
-            return 0;
-        }
-
-        //red nodes cannot have red children
-        if (root->left != NULL && root->left->color == 1) {
-            printf("INSERTION FAILED: Left child of node %d is red\n", root->data);
-            return 0;
-        }
-
-        //red nodes cannot have red children
-        if (root->right != NULL && root->right->color == 1) {
-            printf("INSERTION FAILED: Right child of node %d is red\n", root->data);
-            return 0;
-        }
     }
 
-    //SEG FAULT
-    // //left and right subtree of root must have same number of black nodes
-    // if (leftBlackNum != rightBlackNum) {
-    //     printf("INSERTION FAILED: Uneven number of black nodes starting at node %d\n", root->data);
-    //     return 0;
-    // }
+    //nodes color has to be 1 or 0 (red or black)
+    if(root->color != 1 && root->color != 0) {
+        printf("INSERTION FAILED: Node #%d has an invalid color type", root->data);
+        return 0;
+    }
 
-    // Return the black height of the subtree rooted at the current node
-    // This return is used for our black node check
-    // return (root->color == 0) ? leftBlackNum + 1 : leftBlackNum;
+    //root node MUST be black
+    if(root->color != 0) {
+        printf("INSERTION FAILED: Root node MUST be black", root->data);
+        return 0;
+    }
 
+    //red nodes cannot have red children
+    if (root->left != NULL && root->left->color == 1) {
+        printf("INSERTION FAILED: Left child of node %d is red\n", root->data);
+        return 0;
+    }
+
+    //red nodes cannot have red children
+    if (root->right != NULL && root->right->color == 1) {
+        printf("INSERTION FAILED: Right child of node %d is red\n", root->data);
+        return 0;
+    }
+
+    //checking for same number of black nodes
+    left_black_nodes = check_tree(root->left);
+    right_black_nodes = check_tree(root->right);
+    if (left_black_nodes != right_black_nodes) {
+        printf("INSERTION FAILED: Uneven number of black nodes\n", root->data);
+        return 0;
+    }
+
+    // Return the black height of the subtree rooted at this node
+    return (root->color == 0) ? left_black_nodes + 1 : left_black_nodes;
 }
 
 void test_insert_multi_node() {
@@ -167,22 +162,23 @@ void test_insert_no_nodes() {
 
 
     if (check_tree(root)) {
-        printf("\nPASS: Insert in empty tree passed criteria, Red Black tree is valid\n");
+        printf("\nFAIL: Insert in empty tree passed criteria when Red Black tree should NOT be valid\n");
     } else {
-        printf("\nFAIL: Insert in empty tree failed criteria, Red Black tree is invalid\n");
+        printf("\nPASS: Insert in empty tree failed criteria as expected, Red Black tree is invalid\n");
     }
 
     // Perform in-order traversal to verify tree structure
-    printf("In-order traversal test for one node tree: ");
+    printf("In-order traversal test for one node tree (should be empty): ");
     in_order_traversal(root);
-    printf("\n");
 }
 
 
 int main(void) {
+    printf("SEARCH TESTS:\n");
 	test_search_empty_tree();
 	test_search_one_node_tree();
 	test_search_multiple_node_tree();
+    printf("\nINSERTION TESTS:");
     test_insert_no_nodes();
     test_insert_one_node();
     test_insert_multi_node();
