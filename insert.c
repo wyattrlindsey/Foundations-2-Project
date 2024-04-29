bc9688da9aa47899eb880151c3932cf9eefdfbaf
Rose Hoelzel <hoelzelrose@gmail.com>
2024-04-28 21:15:18 -0400

    fixed insert

--- insert.c
+++ insert.c
@@ -4,42 +4,36 @@
 #include "insert.h"
 
 struct Node* insert(struct Node* root, int data) {
-    // Create a new node with the given data
-    struct Node* newNode = createNew(data);
+    //create a new node
+    Node* new = createNew(data);
 
-    // If the tree is empty, make the new node the root and color it black
+    //if the tree is empty, color new node black
     if (root == NULL) {
-        newNode->color = 0;
-        return newNode;
+        new->color = 0;     //BLACK = 0
+        return new;
     }
 
-    // Traverse the tree to find the appropriate position for the new node
-    struct Node* current = root;
-    struct Node* parent = NULL;
-    while (current != NULL) {
-        parent = current;
-        if (data < current->data)
-            current = current->left;
-        else if (data > current->data)
-            current = current->right;
-        else {
-            // Duplicate value, do nothing
-            free(newNode);
+    //if the tree is not empty, find position for the new node
+    Node* test = root;
+    Node* parent = NULL;
+    while (test != NULL) {
+        parent = test;
+        if (data < test->data) {
+            test = test->left;
+        } else if (data > test->data) {
+            test = test->right;
+        } else {
+            free(new);
             return root;
         }
     }
 
-    // Set the parent of the new node
-    newNode->parent = parent;
-
-    // Attach the new node to its parent
-    if (data < parent->data)
-        parent->left = newNode;
-    else
-        parent->right = newNode;
-
-    // Fix any violations of the red-black tree properties
-    // (not shown in this example)
-
+    //add the new node to the tree
+    new->parent = parent;
+    if (data < parent->data) {
+        parent->left = new;
+    } else {
+        parent->right = new;
+    }
     return root;
 }
